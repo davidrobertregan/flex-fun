86079c03a2b7e171e4498b4e0139185d578ba94d
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-11 21:34:45 -0600

    fetching multiple dogs

--- src/components/FlexContainer.js
+++ src/components/FlexContainer.js
@@ -2,21 +2,23 @@
 import { useState, useEffect } from "react"
 
 function FlexContainer() {
-    const [images, setImages] = useState({})
+    const [images, setImages] = useState([])
 
+    // future option to grab images from a different breed
     const getData = () => {
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("https://dog.ceo/api/breed/bulldog/images/random/10")
         .then(resp => resp.json())
-        .then(data => setImages(data))
+        .then(data => setImages(data.message))
     }
 
     useEffect(getData, [])
 
-    console.log(images)
+    const imgElements = images.map(i => <img key={i} width="100px" src={i} alt="dog"></img>)
+
     return(
         <div>
             <h1>Flex Container</h1>
-            <img src={images.message}></img>
+            {imgElements}
         </div>
     )
 }
