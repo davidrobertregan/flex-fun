c92b05379d37ceaeaa10297116a4aa18e94c9956
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-14 14:32:18 -0600

    potential path forward for manipulating flex parent styles with class names

--- src/components/FlexContainer.js
+++ src/components/FlexContainer.js
@@ -1,9 +1,22 @@
 
 import { useState, useEffect } from "react"
+// https://github.com/JedWatson/classnames
 
 function FlexContainer() {
     const [images, setImages] = useState([])
     // use state to set flex properties... classes? styled components?
+    const [parentClasses, setParentClasses] = useState({
+        justifyContent: "flex-end",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "stretch",
+        alignContent: "normal"
+    })
+
+    const { justifyContent, flexDirection, flexWrap, alignItems, alignContent } = parentClasses
+
+
+    // [justify-content--center]
 
     // future option to grab images from a different breed, or refetch images
     const getData = () => {
@@ -18,8 +31,9 @@ function FlexContainer() {
 
     return(
         <div className="flex-container grid-item">
+            <button onClick={() => setParentClasses({...parentClasses, justifyContent: "flex-start"})}>Flex Start</button>
             <h1>Flex Container</h1>
-            <div className="flex-container__flex-box">
+            <div className={`flex-container__flex-box justify-content--${justifyContent}`}>
                 {imgElements}
             </div>
         </div>
