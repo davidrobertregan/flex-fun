6123a92532d6ac3dc31845aac37d249f957b6193
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-14 14:07:55 -0600

    basic flex container defaultstyles

--- src/components/FlexContainer.js
+++ src/components/FlexContainer.js
@@ -3,22 +3,25 @@ import { useState, useEffect } from "react"
 
 function FlexContainer() {
     const [images, setImages] = useState([])
+    // use state to set flex properties... classes? styled components?
 
-    // future option to grab images from a different breed
+    // future option to grab images from a different breed, or refetch images
     const getData = () => {
-        fetch("https://dog.ceo/api/breed/bulldog/images/random/10")
+        fetch("https://dog.ceo/api/breed/bulldog/images/random/18")
         .then(resp => resp.json())
         .then(data => setImages(data.message))
     }
 
     useEffect(getData, [])
 
-    const imgElements = images.map(i => <img key={i} width="100px" src={i} alt="dog"></img>)
+    const imgElements = images.map(i => <img key={i} className="flex-container__flex-item" width="100px" src={i} alt="dog"></img>)
 
     return(
         <div className="flex-container grid-item">
             <h1>Flex Container</h1>
-            {imgElements}
+            <div className="flex-container__flex-box">
+                {imgElements}
+            </div>
         </div>
     )
 }
