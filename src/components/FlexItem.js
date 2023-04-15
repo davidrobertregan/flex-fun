9c5c0f206667b3a9ab086c8eb0962650b16b649e
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-15 16:00:49 -0600

    set order click handler functional

--- src/components/FlexItem.js
+++ src/components/FlexItem.js
@@ -1,8 +1,14 @@
+import { useState } from "react"
 
 
 function FlexItem({ image, idx }) {
+    const [order, setOrder] = useState(0)
+
+    const handleClick = () => {
+        setOrder((order) => order - 1)
+    }
     return(
-        <div className="flex-container__flex-item">
+        <div className="flex-container__flex-item" onClick={handleClick} style={{order: order}}>
             <div className="flex-container__flex-item-number">{idx}</div>
             <img src={image}></img>
         </div>  
