6b39a86961ef2b90ba62d0c3f2b084811154c01b
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-15 16:22:24 -0600

    order displays on image when user clicks

--- src/components/FlexItem.js
+++ src/components/FlexItem.js
@@ -3,13 +3,20 @@ import { useState } from "react"
 
 function FlexItem({ image, idx }) {
     const [order, setOrder] = useState(0)
+    const [displayOrder, setDisplayOrder] = useState(false)
 
     const handleClick = () => {
         setOrder((order) => order - 1)
+        setDisplayOrder(true)
+        setTimeout(() => {
+            setDisplayOrder(false)
+        }, 1000)
     }
+
     return(
         <div className="flex-container__flex-item" onClick={handleClick} style={{order: order}}>
             <div className="flex-container__flex-item-number">{idx}</div>
+            <div className={`flex-container__flex-item-order ${displayOrder ? '' : 'hide'}`}>{order}</div>
             <img src={image}></img>
         </div>  
     )
