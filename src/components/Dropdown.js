d15d75a0f2de7309228a2539d15bb22ab127de6d
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-19 17:44:01 -0600

    moved sizing to actual image; added parent div for dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,12 +1,20 @@
 
 
 function Dropdown({ property, options, onUpdateProperty }) {
+
+    const titleCaseProperty = () => {
+        const result = property.replace(/([A-Z])/g, " $1");
+        return result.charAt(0).toUpperCase() + result.slice(1);
+    }
+
     return(
         <div className="dropdown-component">
-            <label>{property}</label>
-            <select name={property} onChange={onUpdateProperty}>
-                {options.map(o => <option>{o}</option>)}
-            </select>
+            <div className="dropdown-component__dropdown">
+                <label>{titleCaseProperty()}</label>
+                <select name={property} onChange={onUpdateProperty}>
+                    {options.map(o => <option>{o}</option>)}
+                </select>
+            </div>
         </div>
     )
 }
