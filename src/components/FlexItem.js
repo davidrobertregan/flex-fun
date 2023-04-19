6654c493e12b40479bae2df3d0e47c968fe0cbe7
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-19 16:42:56 -0600

    figured out flex container getting too big on nowrap; random image sizes and useRef

--- src/components/FlexItem.js
+++ src/components/FlexItem.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 // add reset button for order
 
@@ -6,13 +6,16 @@ function FlexItem({ image, idx }) {
     const [order, setOrder] = useState(0)
     const [displayOrder, setDisplayOrder] = useState(false)
 
+    /* I didn't want to change the image size value on each re-render, hence useRef */
+    const imageSize = useRef(['sm', 'med', 'lg'][Math.floor(Math.random() * 3)])
+
     const handleClick = () => {
         setOrder((order) => order - 1)
         setDisplayOrder(true)
     }
 
     return(
-        <div className="flex-container__flex-item" onClick={handleClick} style={{order: order}}>
+        <div className={`flex-container__flex-item flex-container__flex-item--${imageSize.current}`} onClick={handleClick} style={{order: order}}>
             <div className="flex-container__flex-item-number">{idx}</div>
             <div className={`flex-container__flex-item-order ${displayOrder ? '' : 'hide'}`}>{order}</div>
             <img src={image}></img>
