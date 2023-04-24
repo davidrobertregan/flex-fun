2b7aa8aeaf8718b6db0b1bd3da4fb13f6624f1b8
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-24 16:34:24 -0600

    create dropdowns function

--- src/components/FlexOptions.js
+++ src/components/FlexOptions.js
@@ -1,19 +1,22 @@
-import Dropdown from "./Dropdown"
+import Dropdown from "./Dropdown";
 // create series of dropdowns for flex options
 // update parent class obj state on change of drop down to value via callback
 
 function FlexOptions({ onUpdateProperty, parentProperties }) {
-    let dropdownComponents = []
 
-    for(let prop in parentProperties){
-        dropdownComponents.push(<Dropdown property={prop} options={parentProperties[prop]} onUpdateProperty={onUpdateProperty}/>)
+    const createDropdowns = () => {
+        let dropdownComponents = []
+        for(let property in parentProperties){
+            dropdownComponents.push(<Dropdown property={property} options={parentProperties[property]} onUpdateProperty={onUpdateProperty}/>)
+        }
+        return dropdownComponents
     }
 
     return(
         <div className="flex-options grid-item">
             <h1>Flex Options</h1>
             <div className="flex-options__dropdown-components">
-                {dropdownComponents}
+                {createDropdowns()}
             </div>
         </div>
     )
